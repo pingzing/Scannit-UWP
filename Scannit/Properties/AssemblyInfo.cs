3a631b1720c4517c91c5ca5ed837f0833fb3b95c
pingzing <mcalistern@gmail.com>
2017-08-03 14:14:51 +0300

    Rename to Scannit

--- Scannit/Properties/AssemblyInfo.cs
+++ Scannit/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TrippitReader")]
+[assembly: AssemblyTitle("Scannit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TrippitReader")]
+[assembly: AssemblyProduct("Scannit")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
